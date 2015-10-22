30eafb6b2a6acbbb800bd087329eec0cc3a0a16e
HrvojeG <hrvoje.grdic1@gmail.com>
2015-10-22 14:54:26 +0200

    add docs

--- prettifier.rb
+++ prettifier.rb
@@ -1,30 +1,45 @@
+# PRETTIFIER class has method prettify
+# which takes numeric input parameter
+# and outputs the short scale string
+# version of it
+#
+# Author:: Hrvoje Grdic
+# License:: Distributes under the same terms as Ruby
+
 class Prettifier
-  M = 1000000.0
-  B = 1000000000.0
-  T = 1000000000000.0
-  Q = 1000000000000000.0
+  MILLION = 1000000.0
+  BILLION = 1000000000.0
+  TRILLION = 1000000000000.0
+  QUADRILLION = 1000000000000000.0
 
-  def combine(number, scale, suffix)
-    (number / scale).round(1).to_s.chomp('.0')+suffix
-  end
-
+  # Takes number as input and returns string
   def prettify(number)
+    # Raise exception for invalid parameter
     unless number.is_a? Numeric
       raise TypeError, 'Not a numeric type'
     end
 
+    # Handle negative input
     abs_number = number.abs
-    if abs_number >= M
-      if abs_number < B
-        return combine(number, M, 'M')
-      elsif abs_number < T
-        return combine(number, B, 'B')
-      elsif abs_number < Q
-        return combine(number, T, 'T')
+    if abs_number >= MILLION
+      if abs_number < BILLION
+        return combine(number, MILLION, 'M')
+      elsif abs_number < TRILLION
+        return combine(number, BILLION, 'B')
+      elsif abs_number < QUADRILLION
+        return combine(number, TRILLION, 'T')
       else
         raise  ArgumentError, 'Number too big'
       end
     end
+    # Return original value
     number.to_s
+  end
+
+  private
+
+  # Helper method
+  def combine(number, scale, suffix)
+    (number / scale).round(1).to_s.chomp('.0')+suffix
   end
 end
